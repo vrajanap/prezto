d4d84e7ed2d4b68dd454b51da3bb25d98cd6071a
Rajan Ponnappan <vrajanap@gmail.com>
2019-01-14 06:10:04 -0800

    Additional configurtion for i3

---
+++ i3_screenlayout/dual_mon.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output DP-1 --mode 1920x1200 --pos 0x0 --rotate left --output HDMI-1 --primary --mode 1920x1080 --pos 1200x760 --rotate normal --output eDP-1 --off --output HDMI-2 --off
---
+++ i3_screenlayout/single_mon.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output DP-1 --off --output HDMI-1 --off --output eDP-1 --primary --mode 1600x900 --pos 0x0 --rotate normal --output HDMI-2 --off
